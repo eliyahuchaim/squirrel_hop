d1552322f02c74ca5c5c91deaaa3f40f87cef3d1
github username <github email address>
2017-07-11 13:16:23 -0400

    vendor login created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :current_vendor
+
   def current_user
     session[:user_id] ||= nil
   end
+
+  def current_vendor
+    session[:vendor_id] ||= nil
+  end
+
 end
--- app/controllers/vendor_sessions_controller.rb
+++ app/controllers/vendor_sessions_controller.rb
@@ -1,2 +1,22 @@
 class VendorSessionsController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+    @vendor = Vendor.find_by(:username, params[:username])
+    if @vendor.authenticate(params[:password])
+      session[:vendor_id] = @vendor.id
+      redirect_to vendor_path(@vendor)
+    else
+      redirect_to vendor_login_path
+  end
+
+  def destroy
+    session[:vendor_id] = nil
+    redirect_to root_path
+  end
+
+
 end
